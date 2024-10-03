afccf0f4709d1868ba8571c595d5e1649553a203
Rodnie <rodnixcaracenar@gmail.com>
2024-10-03 16:45:36 +0800

    My first git

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SystemProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
